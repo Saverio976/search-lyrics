22e870bfaba52eb0cbb72baa3e1bd1d1ea4d1641
Xavier Mitault <xavier.mitault@epitech.eu>
2023-01-08 12:56:02 +0100

    Print song at the end

--- search_lyrics/entrypoint.py
+++ search_lyrics/entrypoint.py
@@ -8,4 +8,6 @@ def entrypoint():
     parser.add_argument("--genius-access-token", nargs=1, metavar="genius_access_token", type=str, default=[None], help="access token to use for genius api (by default, search for GENIUS_ACCESS_TOKEN in ENV variables)")
     parser.add_argument("--verbose", action="store_const", const=True, default=False, help="Enable verbose output")
     args = parser.parse_args()
-    search_lyrics(args.artist[0], args.text[0], args.genius_access_token[0], args.verbose)
+    songs = search_lyrics(args.artist[0], args.text[0], args.genius_access_token[0], args.verbose)
+    for song in songs:
+        print(song.title)
